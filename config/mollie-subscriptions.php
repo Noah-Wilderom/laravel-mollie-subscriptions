72722d7fae4486dfc9aecf7a9a9c16060e0e44dd
noahwilderom <noahwilderom@users.noreply.github.com>
2023-03-12 01:05:28 +0000

    Fix styling

--- config/mollie-subscriptions.php
+++ config/mollie-subscriptions.php
@@ -7,6 +7,6 @@ return [
 
     'database' => [
         'uuid' => false,
-        ''
-    ]
+        '',
+    ],
 ];
--- src/Traits/Subscribable.php
+++ src/Traits/Subscribable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace NoahWilderom\LaravelMollieSubscriptions;
 
 trait Subscribable
